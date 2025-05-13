d413bf958eaf6f86e083496ea2cff0746e10bc23
AlirezzA-dev <alirezaalidokht1387@gmail.com>
2025-05-13 20:11:03 +0330

    update setting allowed hosts

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -24,12 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kezrqqmr6s$s!#u3whsld$p6yph$$ih1p@=knr9o21e197&pjr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['']
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ALLOWED_HOSTS = ['roomeet.onrender.com']
 
 # Application definition
 
@@ -120,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
