c59fa63a26e098b6171909bcdaad1837ebca0ec8
AlirezzA-dev <alirezaalidokht1387@gmail.com>
2025-05-13 20:18:31 +0330

    fix home view

--- chatproject/urls.py
+++ chatproject/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
+def home_view(request):
+    return HttpResponse("Done!")
+
 urlpatterns = [
+    path('', home_view),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
